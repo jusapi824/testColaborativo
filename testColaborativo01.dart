4f9557a22ae186d0c15f9ed3336ed2759b022638
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-06-18 15:02:25 -0500

    feat: new functionality to show

--- testColaborativo01.dart
+++ testColaborativo01.dart
@@ -1,3 +1,9 @@
 void main(List<String> args) {
   print("Test Colaborativo 01");
+
+  List<int> numeros = [1, 2, 3, 4, 5 ,6 ,7, 8];
+  //Mostrar elementos
+  for (var i = 0; i < numeros.length; i++) {
+    print(numeros);
+  }
 }
