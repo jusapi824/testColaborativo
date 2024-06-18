cc262601207791341ba4c92ee6457546aca35599
teoxs01 <mateogarcua451@gmail.com>
2024-06-18 15:15:37 -0500

    feat:New funtionality to search a number

--- testColaborativo01.dart
+++ testColaborativo01.dart
@@ -1,9 +1,16 @@
 void main(List<String> args) {
   print("Test Colaborativo 01");
 
-  List<int> numeros = [1, 2, 3, 4, 5 ,6 ,7, 8];
+  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8];
   //Mostrar elementos
   for (var i = 0; i < numeros.length; i++) {
     print(numeros);
+  }
+  //Buscar numero dentro del vector
+  int numeroBuscar = 4;
+  for (var i = 0; i < numeros.length; i++) {
+    if (numeroBuscar == numeros[i]) {
+      print("numero se encuentra en la posicion $i");
+    }
   }
 }
